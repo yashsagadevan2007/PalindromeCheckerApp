cb987311e9cffa07615f323e6d599d8d5ecd384a
yashsagadevan2007 <yashsagadevan2007@gmail.com>
2026-03-06 11:00:42 +0530

    UC6:Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class UseCase5PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
@@ -11,21 +13,26 @@ public class UseCase5PalindromeCheckerApp {
         System.out.print("Enter a string to check if it is a palindrome: ");
         String input = scanner.nextLine();
 
-        // Create a stack to store characters
+        // Create Stack and Queue
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push each character of the string into the stack
+        // Push and Enqueue characters
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop characters and compare with original string
-        for (int i = 0; i < input.length(); i++) {
-            char poppedChar = stack.pop();
+        // Compare pop from stack and dequeue from queue
+        while (!stack.isEmpty() && !queue.isEmpty()) {
 
-            if (input.charAt(i) != poppedChar) {
+            char fromStack = stack.pop();     // reverse order
+            char fromQueue = queue.remove();  // original order
+
+            if (fromStack != fromQueue) {
                 isPalindrome = false;
                 break;
             }
